5c3e008bd7bfdeb42a403f2dfe1a4191b7facd05
raT <titustshanon@gmail.com>
2025-11-27 12:40:09 +0800

    Done, Final

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import Form from './Form'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
+  const [theme, setTheme] = useState('light');
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Form theme={theme} />
+      <label>
+        <input
+          type="checkbox"
+          checked={theme === 'dark'}
+          onChange={(e) => {
+            setTheme(e.target.checked ? 'dark' : 'light');
+          }}
+        />
+        Use dark mode
+      </label>
     </>
-  )
+  );
 }
-
-export default App
